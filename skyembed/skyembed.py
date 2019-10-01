5fbf0dff76d74bcfc4b25740332d0c6db172121d
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-10-01 02:51:17 -0400

    Update skyembed.py

--- skyembed/skyembed.py
+++ skyembed/skyembed.py
@@ -40,6 +40,18 @@ class Embed(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
+    
+    @bot.event
+    async def on_message(message):
+        guild = message.guild
+        if guild:
+            path = "chatlogs/{}.txt".format(guild.id)  
+            with open(path, 'a+') as f:
+                print("{0.timestamp} : {0.author.name} : {0.content}".format(message), file=f)
+        await bot.process_commands(message)
+
+
     @commands.command(
         name='sembed',
         description='The embed command',
