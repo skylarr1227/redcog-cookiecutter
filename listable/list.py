cb683b3f2b4a6f70eb6ac9e22b46a9133080c1a2
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-10-02 17:59:05 -0400

    Create list.py

---
+++ listable/list.py
@@ -0,0 +1,13 @@
+from os import getenv
+import discord
+from discord.ext import commands
+
+
+
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    '''Returns pong when called'''
+    author = ctx.message.author.name
+    server = ctx.message.server.name
+    await bot.say('Pong for {} from {}!'.format(author, server))
