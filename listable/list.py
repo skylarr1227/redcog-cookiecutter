fbcfaf00cf768718becc4a7bb9ef0c0a97b90f29
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-10-02 20:55:17 -0400

    Update list.py

--- listable/list.py
+++ listable/list.py
@@ -9,7 +9,7 @@ from redbot.core import Config, checks, commands
 from redbot.core.utils import chat_formatting as cf
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 
-From models import Base, Event, Member, Attendance
+From models import Base, List, Member, Yes
 
 
 BaseCog = getattr(commands, "Cog", object)
@@ -24,9 +24,8 @@ class Listable(BaseCog)
     def __init__(self, bot):
         self.bot = bot
 
-
     @bot.command(pass_context=True)
-    async def ping(ctx):
+    async def pong(ctx):
         '''Returns pong when called'''
         author = ctx.message.author.name
         server = ctx.message.server.name
@@ -35,17 +34,17 @@ class Listable(BaseCog)
 
     @bot.command(pass_context=True)
     async def create(ctx, name: str, date: str, time: str='0:00am'):
-        '''Creates an event with specified name and date
+        '''Creates an list with specified name and date
             example: ?create party 12/22/2017 1:40pm
         '''
         server = ctx.message.server.name
         date_time = '{} {}'.format(date, time)
         try:
-            event_date = datetime.strptime(date_time, '%m/%d/%Y %I:%M%p')
-            event = Event(name=name, server=server, date=event_date)
-            session.add(event)
+            list_date = datetime.strptime(date_time, '%m/%d/%Y %I:%M%p')
+            list = List(name=name, server=server, date=list_date)
+            session.add(list)
             session.commit()
-            await bot.say('Event {} created successfully for {}'.format(name, event.date))
+            await bot.say('List {} created successfully for {}'.format(name, list.date))
         except Exception as e:
             await bot.say('Could not complete your command')
             print(e)
@@ -53,9 +52,9 @@ class Listable(BaseCog)
 
 
     @bot.command(pass_context=True)
-    async def attend(ctx, name: str):
-       '''Allows a user to attend an upcoming event
-             example: ?attend party
+    async def checkin(ctx, name: str):
+       '''Allows a user to checkin on a list
+             example: +checkin skysparty
        '''
         author = ctx.message.author.name
         avatar = ctx.message.author.avatar_url
@@ -63,11 +62,11 @@ class Listable(BaseCog)
 
         try:
             count = session.query(Member).filter(Member.id == id).count()
-            event = session.query(Event).filter(Event.name == name).first()
+            list = session.query(List).filter(List.name == name).first()
 
-            # Verify This event exists
-            if not event:
-                await bot.say('This event does not exist')
+            # Verify This list exists
+            if not list:
+                await bot.say('This list does not exist')
                 return
 
             # Create member if they do not exist in our database
@@ -75,23 +74,23 @@ class Listable(BaseCog)
                 member = Member(id=id, name=author, avatar=avatar)
                 session.add(member)
 
-            attending = Attendance(member_id=id, event_id=event.id)
-            session.add(attending)
+            checkedin = Yes(member_id=id, list_id=list.id)
+            session.add(checkedin)
             session.commit()
-            await bot.say('Member {} is now attending event {}'.format(author, name))
+            await bot.say('Member {} has checked in on list {}'.format(author, name))
         except Exception as e:
             await bot.say('Could not complete your command')
             print(e)
 
     @bot.command()
-    async def list():
-        '''Displays the list of current events
-            example: ?list
+    async def showlists():
+        '''Displays the list of all the lists
+            example: +showlist
         '''
         try:
-            events = session.query(Event).order_by(Event.date).all()
+            lists = session.query(List).order_by(List.date).all()
             headers = ['Name', 'Date', 'Server']
-            rows = [[e.name, e.date, e.server] for e in events]
+            rows = [[e.name, e.date, e.server] for e in lists]
             table = tabulate(rows, headers)
             await bot.say('```\n' + table + '```')
             except Exception as e:
@@ -100,18 +99,18 @@ class Listable(BaseCog)
 
     @bot.command()
     async def view(name: str):
-        '''Displays information about a specific event
-            example: ?view party
+        '''Displays information about a specific list
+            example: +view list
         '''
         try:
-        event = session.query(Event).filter(Event.name == name).first()
-        # Verify This event exists
-        if not event:
-            await bot.say('This event does not exist')
+        list = session.query(List).filter(List.name == name).first()
+        # Verify This list exists
+        if not list:
+            await bot.say('This list does not exist')
             return
 
-            attending = session.query(Attendance).filter(Attendance.event_id == event.id).count()
-            info = [['Name', event.name], ['Date', event.date], ['Server', event.server], ['Number Attending', attending]]
+            checkedin = session.query(Yes).filter(Yes.list_id == list.id).count()
+            info = [['Name', list.name], ['Date', list.date], ['Server', list.server], ['Number checkedin', Yes]]
             await bot.say('```\n' + tabulate(info) + '```')
         except Exception as e:
             await bot.say('Could not complete your command')
