0c10d3792435a7e9d48839d25d8c5b666f8ab1e1
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-10-28 10:21:25 -0400

    Update skyembed.py

--- skyembed/skyembed.py
+++ skyembed/skyembed.py
@@ -36,65 +36,6 @@ colors = {
   'NOT_QUITE_BLACK': 0x23272A
 }
 
-bot = commands.Bot
-BaseCog = getattr(commands, "Cog", object)
-listener = getattr(commands.Cog, "listener", None) 
-if listener is None:
-  
-    def listener(name=None):
-        return lambda x: x
-    
-class Skyembed(commands.Cog):
-
-    def __init__(self, bot):
-        self.bot = bot
-    
-
-    @commands.command(pass_context=True)
-    @checks.is_owner()
-    async def partycrash(self, ctx, idnum=None):
-        """Lists servers and generates invites for them"""
-        owner = ctx.message.author
-        if idnum:
-            server = discord.utils.get(self.bot.Guilds, id=idnum)
-            if server:
-                await self._confirm_invite(server, owner, ctx)
-            else:
-                await self.bot.say("I'm not in that server")
-        else:
-            msg = ""
-            servers = sorted(self.bot.Guilds, key=lambda s: s.name)
-            for i, server in enumerate(servers, 1):
-                msg += "{}: {}\n".format(i, Guild.name)
-            msg += "\nTo post an invite for a server just type its number."
-            for page in pagify(msg, delims=["\n"]):
-                await self.bot.say(box(page))
-                await asyncio.sleep(1.5)  # Just in case for rate limits
-            msg = await self.bot.wait_for_message(author=owner, timeout=15)
-            if msg is not None:
-                try:
-                    msg = int(msg.content.strip())
-                    server = servers[msg - 1]
-                except ValueError:
-                    await self.bot.say("You must enter a number.")
-                except IndexError:
-                    await self.bot.say("Index out of range.")
-                else:
-                    try:
-                        await self._confirm_invite(server, owner, ctx)
-                    except discord.Forbidden:
-                        await self.bot.say("I'm not allowed to make an invite"
-                                           " for {}".format(Guild.name))
-            else:
-                await self.bot.say("Response timed out.")
- 
-
-
-    @commands.command(name='joinsrv', description='send invite for discord server')
-    async def create_invite(self, ctx, channel: int = None):
-        """Create instant invite"""
-        link = await ctx.channel.create_invite(max_age = 300)
-        await ctx.send(link)
 
     #@commands.command(name='joinsrv', description='send invite for discord server')
    # async def joinsrv(self, ctx):
