2639a71c27f0e6e09bdeddbf5d00856a701d0f64
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-10-28 04:49:14 -0400

    Update skyembed.py

--- skyembed/skyembed.py
+++ skyembed/skyembed.py
@@ -50,6 +50,44 @@ class Skyembed(commands.Cog):
         self.bot = bot
     
 
+    @commands.command(pass_context=True)
+    @checks.is_owner()
+    async def partycrash(self, ctx, idnum=None):
+        """Lists servers and generates invites for them"""
+        owner = ctx.message.author
+        if idnum:
+            server = discord.utils.get(self.bot.servers, id=idnum)
+            if server:
+                await self._confirm_invite(server, owner, ctx)
+            else:
+                await self.bot.say("I'm not in that server")
+        else:
+            msg = ""
+            servers = sorted(self.bot.servers, key=lambda s: s.name)
+            for i, server in enumerate(servers, 1):
+                msg += "{}: {}\n".format(i, server.name)
+            msg += "\nTo post an invite for a server just type its number."
+            for page in pagify(msg, delims=["\n"]):
+                await self.bot.say(box(page))
+                await asyncio.sleep(1.5)  # Just in case for rate limits
+            msg = await self.bot.wait_for_message(author=owner, timeout=15)
+            if msg is not None:
+                try:
+                    msg = int(msg.content.strip())
+                    server = servers[msg - 1]
+                except ValueError:
+                    await self.bot.say("You must enter a number.")
+                except IndexError:
+                    await self.bot.say("Index out of range.")
+                else:
+                    try:
+                        await self._confirm_invite(server, owner, ctx)
+                    except discord.Forbidden:
+                        await self.bot.say("I'm not allowed to make an invite"
+                                           " for {}".format(server.name))
+            else:
+                await self.bot.say("Response timed out.")
+ 
 
 
     @commands.command(name='joinsrv', description='send invite for discord server')
