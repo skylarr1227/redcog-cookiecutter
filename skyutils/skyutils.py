c6828f65b2eb30dbc81bbccffa886237e39e1092
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-12-10 02:06:53 -0500

    Update skyutils.py

--- skyutils/skyutils.py
+++ skyutils/skyutils.py
@@ -65,89 +65,32 @@ class Skyutils(commands.Cog):
         await paginator.run()
     
     
-    @commands.command()
-    async def sayit(self, ctx, *, args: str):
-        """
-        Edit/Say a message and you can use the silentsay variable as well.
-        **FLAGS**:``
-            If you use `--s` as a flag, your arguments are simply what you want the bot to say and have it remove your message afterwards.
-            If you use `--e` as a flag, the arguments are the message id you want to edit and the new content.
-            If no flag is passed, the bot will simply send the message like normal excluding the actual command."""
-        args = args.replace("@everyone", "<insert {} trying to mention everyone here>".format(str(ctx.message.author)))
-        args = args.replace("@here", "<insert {} trying to mention everyone here>".format(str(ctx.message.author)))
-        if args.startswith("--s"):
-            if args == "--s":  # --s == silentsay
-                await ctx.send("You can't silently send an empty message!")
-            else:
-                args = args[4:]
-                await ctx.send(str(args))
-                try:
-                    await ctx.message.delete()
-                except:
-                    pass
-        elif args.startswith("--e"):
-            if args == "--e":  # --e == edit
-                return await ctx.send("You can't edit nothingness.")
-            try:
-                args = args[4:]
-                mid = args.split(' ')
-                to_edit = await ctx.message.channel.get_message(int(mid[0]))
-                content = args.replace(mid[0] + " ", "")
-                try:
-                    await to_edit.edit(content=content)
-                except discord.Forbidden:
-                    await ctx.send("Can't edit that message.")
-                await ctx.message.delete()
-            except discord.NotFound:
-                not_found = await ctx.send("Couldn't find the message.")
-                await asyncio.sleep(5)
-                await not_found.delete()
-                await ctx.message.delete()
-            except (IndexError, ValueError):
-                indx_err = await ctx.send("Usage: `g_say --e <id> <content>`")
-                await asyncio.sleep(5)
-                await indx_err.delete()
-                await ctx.message.delete()
-            except discord.Forbidden:
-                err = await ctx.send("I don't have permission to delete your message.")
-                await asyncio.sleep(5)
-                await err.delete()
-        else:
-            await ctx.send(str(args))
+    
             
             
+
     @commands.command()
-    async def calc(self, ctx, *, expression: str):
-        """Evaluate complex mathematical equations (or simple ones, whatever you prefer).
-        The available operations are as follows:
-        `simplify, factor, derive, integrate, zeroes, tangent, area, cos, tan, arccos, arcsin, arctan, abs, log`"""
-        available_endpoints = ["simplify", "factor", "derive", "integrate", "zeroes", "tangent", "area", "cos", "tan",
-                               "arccos", "arcsin", "arctan", "abs", "log"]
-        oper = expression.split(' -operation ')
-        op = "simplify"
-        if len(oper) > 1:
-            try:
-                if oper[1].lower() in available_endpoints:
-                    op = oper[1].lower()
-                else:
-                    return await ctx.send(resolve_emoji('ERROR', ctx) + " S-Sorry! That operation seems invalid")
-            except:
-                return await ctx.send(
-                    resolve_emoji('ERROR',
-                                  ctx) + " Y-you need to give me a valid operation! I made a list for you in the command help.")
-        expr = oper[0].replace('/', '%2F')
-        r = requests.get("https://newton.now.sh/" + op + "/" + expr)
-        try:
-            js = r.json()
-        except json.decoder.JSONDecodeError:
-            return await ctx.send(resolve_emoji('ERROR', ctx) + " I-I'm sorry! Something happened with the api.")
-        em = discord.Embed(title="Expression Evaluation", color=ctx.message.author.color)
-        em.add_field(name="Operation", value=js['operation'], inline=False)
-        em.add_field(name="Expression", value=js['expression'], inline=False)
-        em.add_field(name="Result", value=js['result'], inline=False)
-        em.set_footer(text="Requested by " + str(ctx.message.author))
-        em.timestamp = datetime.datetime.now()
-        await ctx.send(embed=em)
+    async def add(self, ctx, left: int, right: int):
+        """Dem math probs with the + in em"""
+        await ctx.send(left + right)
+
+    @commands.command()
+    async def subtract(self, ctx, left: int, right: int):
+        """For all those times you can't be bothered with
+        how many apples Jake has after little Timmy used 6
+        to throw at the kids doing their math homework"""
+        await ctx.send(left - right)
+
+    @commands.command()
+    async def multiply(self, ctx, left: int, right: int):
+        """fuck a calculator, skybot is here baby"""
+        await ctx.send(left * right)
+
+    @commands.command()
+    async def divide(self, ctx, left: int, right: int):
+        """Division is for nerds"""
+        await ctx.send(left / right)
+
         
   
             
