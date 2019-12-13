9aedc83ed9f89022b992d1c50a6d64039c32e49a
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-12-13 11:09:02 -0500

    Update skyutils.py
    added math

--- skyutils/skyutils.py
+++ skyutils/skyutils.py
@@ -115,5 +115,42 @@ class Skyutils(commands.Cog):
         else:
             await ctx.send(':x: You don\'t have permission to change your nickname.')      
             
+            
+    @commands.command()
+    async def emoteinfo(self, ctx, emoji : FailsafeEmojiConverter):
+        """Retrieves information about an emoji."""
+        e = discord.Embed(type='rich', color=blurple)
+        if isinstance(emoji, discord.Emoji):
+            url = emoji.url.replace('discordapp.com/api', 'cdn.discordapp.com')
+            e.set_thumbnail(url=url)
+            e.add_field(name='Name', value=emoji.name)
+            e.add_field(name='ID', value=emoji.id)
+            e.add_field(name='Created at', value=emoji.created_at.strftime(datetime_format))
+            e.add_field(name='URL', value=url)
+        else:
+            e.add_field(name='Name', value=unicodedata.name(emoji))
+            e.add_field(name='ID', value='Built-in')
+            await ctx.send(embed=e)
+            
+    @commands.Cog.listener()
+    async def on_message(self,message):
+        mathshit=['+','-','*','/','^']
+        msg=message.content
+        msgshit=msg.split(' ')
+        for a in msgshit:
+            if a in mathshit:
+                if a=='+':
+                    await message.channel.send(str(int(msgshit[0])+int(msgshit[2])))
+                elif a=='-':
+                    await message.channel.send(str(int(msgshit[0])-int(msgshit[2])))
+                elif a=='*':
+                    await message.channel.send(str(int(msgshit[0])*int(msgshit[2])))
+                elif a=='/':
+                    await message.channel.send(str(int(msgshit[0])/int(msgshit[2])))
+                elif a =='^':
+                    await message.channel.send(str(int(msgshit[0])**int(msgshit[2])))
+                else:
+                    print('whatever')
+                   
 def setup(bot):
     bot.add_cog(Skyutils(bot))
