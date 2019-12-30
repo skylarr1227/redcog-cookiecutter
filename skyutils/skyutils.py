9ca30fecb54e0c56391faa14e0616c5badfa0785
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-12-30 02:14:15 -0500

    Update skyutils.py

--- skyutils/skyutils.py
+++ skyutils/skyutils.py
@@ -168,6 +168,7 @@ class Skyutils(commands.Cog):
         await msg.add_reaction(emoji)
         reaction, user = await self.bot.wait_for('reaction_add', check=check)
         await ctx.channel.send (str(user))
+        await ctx.send(f"<@{str(user.id)}>")
             
            
     @commands.command(pass_context=True)
